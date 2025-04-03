076160f3a3af7fdd6e794995af86a51a1ce7af15
Gyanendra Sahoo <gyanendrasahoo577@gmail.com>
2025-04-04 05:07:23 +0530

    Gender Section Frontend

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
     <section className='relative'>
-        <img src={heroImg} alt="Rabbit" className='w-full h-[400px] md:h-[600] lg:h-[750px] object-cover' />
-        <div className='absolute inset-0 bg-black bg-opacity-5 flex items-center justify-center'>
-            <div className='text-center text-white p-6'>
-                <h1 className='text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4'>Vaccation<br/>Ready</h1>
-                <p className='text-sm tracking-tighter md:text-lg mb-6'>
-                    Explore our vacation-ready outfits with fast worldwide shipping.
-                </p>
-                <Link to="#" className='bg-white text-gray-950 px-6 py-2 rounded-sm text-lg'>Shop Now</Link>
-            </div>
+      <img src={heroImg} alt="Rabbit" className='w-full h-[400px] md:h-[600px] lg:h-[750px] object-cover' />
+      <div className='absolute inset-0 flex items-center justify-center'> {/* Changed bg-opacity-5 to opacity-5 */}
+        <div className='text-center text-white p-6'>
+          <h1 className='text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4 '>Vacation<br />Ready</h1>
+          <p className='text-sm tracking-tighter md:text-lg mb-6'>
+            Explore our vacation-ready outfits with fast worldwide shipping.
+          </p>
+          <Link to="#" className='bg-white text-gray-950 px-6 py-2 rounded-sm text-lg'>Shop Now</Link>
         </div>
+      </div>
     </section>
   )
 }
--- frontend/src/components/Products/GenderCollectionSection.jsx
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import mensCollectionsImage from '../../assets/mens-collection.webp'
+import womensCollectionsImage from '../../assets/womens-collection.webp'
 
 const GenderCollectionSection = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='py-16 px-4 lg:px-0'>
+      <div className='container mx-auto flex flex-col md:flex-row gap-8'>
+        {/* Women's Collection */}
+        <div className='relative flex-1'>
+          <img src={womensCollectionsImage} alt="Women's Collection" className='w-full h-[700px] object-cover' />
+          <div className='absolute bottom-8 left-8 bg-white bg-opacity-90 p-4'>
+            <h2 className='text-2xl font-bold text-gray-900 mb-3'>
+              Women's Collection
+            </h2>
+            <Link to="/collections/women" className='text-gray-900 underline'>Shop Now</Link>
+          </div>
+        </div>
+        {/* Mens'Collection */}
+        <div className='relative flex-1'>
+          <img src={mensCollectionsImage} alt="Men's Collection" className='w-full h-[700px] object-cover' />
+          <div className='absolute bottom-8 left-8 bg-white bg-opacity-90 p-4'>
+            <h2 className='text-2xl font-bold text-gray-900 mb-3'>
+              Men's Collection
+            </h2>
+            <Link to="/collections/men" className='text-gray-900 underline'>Shop Now</Link>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
